36f56d64df926852e213ba78e4ea67b63d085350
Adella Desinta <adelladesinta88@gmail.com>
2023-10-18 18:25:27 +0800

    create api crud user and shop

---
+++ controller/shopController.js
@@ -0,0 +1,113 @@
+const { Shop } = require("../models");
+const imagekit = require("../lib/imagekit");
+const ApiError = require("../utils/apiError");
+
+const createShop = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    const newShop = await Shop.create({
+      name,
+    });
+
+    res.status(201).json({
+      status: "Success",
+      data: {
+        newShop,
+      },
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+const findShops = async (req, res, next) => {
+  try {
+    const shops = await Shop.findAll();
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        shops,
+      },
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+const findShopById = async (req, res, next) => {
+  try {
+    const shop = await Shop.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        shop,
+      },
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+const updateShop = async (req, res, next) => {
+  const { name, price, stock } = req.body;
+  try {
+    await Shop.update(
+      {
+        name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    res.status(200).json({
+      status: "Success",
+      message: "sukses update produk",
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+const deleteShop = async (req, res, next) => {
+  try {
+    const shop = await Shop.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!shop) {
+      next(new ApiError("Shop id tersebut gak ada", 404));
+    }
+
+    await Shop.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({
+      status: "Success",
+      message: "sukses delete shop",
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+module.exports = {
+  createShop,
+  findShops,
+  findShopById,
+  updateShop,
+  deleteShop,
+};
